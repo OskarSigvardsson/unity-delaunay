77eee3f639bbcdeb02fe1c49ffa85e7695d53e18
Oskar Sigvardsson <oskar.sigvardsson@groplay.com>
2017-07-25 14:52:21 +0200

    Initial project commit

---
+++ Assets/DebugDelaunay.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GK {
+	public class DebugDelaunay : MonoBehaviour {
+
+		public UnityEngine.UI.Text Text;
+
+		IEnumerator Start() {
+			yield return new WaitForSeconds(3.0f);
+			//var p = new Vector2(8.9f, 3.5f);
+			//var c0 = new Vector2(-118.5f, -14.2f);
+			//var c1 = new Vector2(123.9f, -14.2f);
+			//var c2 = new Vector2(3.2f, 125.7f);
+
+			//Debug.Log(Geom.Contains(p, c0, c1, c2));
+			//return;
+
+			var del = new Delaunay();
+			var points = new List<Vector2>();
+
+			while (true) {
+				points.Clear();
+				var count = 1000;
+
+				for (int i = 0; i < count; i++) {
+					points.Add(new Vector2(Random.value, Random.value));
+				}
+
+				var t = System.DateTime.UtcNow;
+				del.SetPoints(points);
+				del.Calculate();
+				Text.text = "" + (System.DateTime.UtcNow - t).TotalMilliseconds;
+
+				yield return new WaitForSeconds(0.5f);
+			}
+		}
+	}
+}
---
+++ Assets/Plugins/Delaunay/Geom.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GK {
+	public class Geom : MonoBehaviour {
+
+		public static bool ToTheLeft(Vector2 p, Vector2 l0, Vector2 l1) {
+			return ((l1.x-l0.x)*(p.y-l0.y) - (l1.y-l0.y)*(p.x-l0.x)) >= 0;
+		}
+
+		public static bool Contains(Vector2 p, Vector2 c0, Vector2 c1, Vector2 c2) {
+			return ToTheLeft(p, c0, c1)
+				&& ToTheLeft(p, c1, c2)
+				&& ToTheLeft(p, c2, c0);
+		}
+
+		public static bool InsideCircumcircle(Vector2 p, Vector2 c0, Vector2 c1, Vector2 c2) {
+			var ax = c0.x-p.x;
+			var ay = c0.y-p.y;
+			var bx = c1.x-p.x;
+			var by = c1.y-p.y;
+			var cx = c2.x-p.x;
+			var cy = c2.y-p.y;
+
+			return (
+					(ax*ax + ay*ay) * (bx*cy-cx*by) -
+					(bx*bx + by*by) * (ax*cy-cx*ay) +
+					(cx*cx + cy*cy) * (ax*by-bx*ay)
+			) > 0;
+		}
+	}
+}
